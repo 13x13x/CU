1c783c477abd76f95220c973dd3cf1286e80729d
13x13x <153285185+13x13x@users.noreply.github.com>
2025-02-16 12:33:10 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,10 +2,10 @@
 
 const BOT_TOKEN = "8006465305:AAEwHDDKXzl95QGtFE4ZwiMrBcfq3TUiZgY";
 const BOT_WEBHOOK = "/endpoint";
-const BOT_SECRET = "nothing123@";
+const BOT_SECRET = "nothing123";
 const BOT_OWNER = 5549620776;
 const BOT_CHANNEL = -1002288959081;
-const SIA_SECRET = "Nice123@";
+const SIA_SECRET = "Nice123";
 const PUBLIC_BOT = false;
 
 // ---------- Do Not Modify ---------- // 
